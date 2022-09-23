9e8b2ebc4ac385c35db64f8e8a169418cdddf6e5
Tung Le <tung.le0319@gmail.com>
2022-09-23 12:06:43 -0600

    tung update 2

---
+++ client/app/Models/ActivePost.js
@@ -0,0 +1,11 @@
+export class ActivePost{
+  constructor(data){
+
+    this.id = data.id
+    this.name = data.name
+    this.imgUrl = data.imgUrl
+    this.location = data.location 
+    this.description = data.description
+  }
+
+}
--- client/app/Models/Cryptid.js
+++ client/app/Models/Cryptid.js
@@ -2,10 +2,11 @@ export class Cryptid {
 
 
   constructor(data) {
+    this.id = data.id
 this.name = data.name
-this.imgUrl = data.imUrl
+this.imgUrl = data.imgUrl
 this.location = data.location
-this.description = data.location
+this.description = data.description
 
   }
 
@@ -14,25 +15,29 @@ this.description = data.location
       <div class="card elevation-3 mx-4 my-2" style="width: 17.5rem">
               <div class="d-flex justify-content-center">
                 <img
-                  src="https://d2ph5fj80uercy.cloudfront.net/04/cat3676.jpg"
+                  src="${this.imgUrl}"
                   alt=""
                   class="img-fluid rounded mt-2 elevation-4 polaroid"
                 />
               </div>
               <div class="card-body">
-                <div class="border-bottom border-dark border-3">
+                <div class="border-bottom border-dark border-1">
                   <span> <i class="mdi mdi-comment fs-2 selectable"  data-bs-toggle="modal"
-              data-bs-target="#postModal"></i></span>
+              data-bs-target="#postModal" onclick="app."></i><small>6</small></span>
                   <span class="mx-3">
-                    <i class="mdi mdi-arrow-up-box fs-2 selectable"></i>14</span
+                    <i class="mdi mdi-arrow-up-box text-success fs-4 selectable"></i><small>14</small></span
                   >
                   <span>
-                    <i class="mdi mdi-arrow-down-box fs-2 selectable"></i
-                    >5</span
+                    <i class="mdi mdi-arrow-down-box text-danger fs-4 selectable"></i
+                    ><small>5</small></span
                   >
                 </div>
-                <div class="d-flex justify-content-end text-shadow mt-2">
-                  <h6>PlaceHolder Text</h6>
+                <div class="text-shadow mt-2">
+                <div>
+                </div>
+                  <h5 class="text-decoration-underline">${this.name}</h5>
+                  <p>${this.location}</p>
+                  <small>${this.description}</small>
                 </div>
               </div>
             </div>
@@ -40,4 +45,6 @@ this.description = data.location
    
     `;
   }
+
+  
 }
