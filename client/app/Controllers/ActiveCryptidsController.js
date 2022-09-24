f99b3e6c222928448f1af7631a766c0e9829c3bc
Tung Le <tung.le0319@gmail.com>
2022-09-23 22:50:17 -0600

    tung

--- client/app/Controllers/ActiveCryptidsController.js
+++ client/app/Controllers/ActiveCryptidsController.js
@@ -1,33 +1,21 @@
-import { appState } from "../AppState.js";
-import { cryptidsService } from "../Services/CryptidsService.js";
-import { Pop } from "../Utils/Pop.js";
-import { setHTML } from "../Utils/Writer.js";
+import { appState } from '../AppState.js';
+import { cryptidsService } from '../Services/CryptidsService.js';
+import { Pop } from '../Utils/Pop.js';
+import { setHTML } from '../Utils/Writer.js';
 
 function drawActiveCryptid() {
   if (appState.activeCryptids == null) {
-    return
+    return;
   }
 
-
-  setHTML('activecryptid', appState.activeCryptids.CommentsModalTemplate)
-}
-
-
-function drawComments() {
-  let template = ''
-  let leftover = appState.comments.filter(c => c.cryptidId == appState.activeCryptids.id)
-  leftover.forEach(c => template += c.CommentTemplate)
-  setHTML('active-comments', template)
+  setHTML('activecryptid', appState.activeCryptids.CommentsModalTemplate);
 }
 
 export class ActiveCryptidsController {
   constructor() {
     appState.on('activeCryptids', drawActiveCryptid);
-    // appState.on('activeCryptids', drawComments);
-    // appState.on('activeCryptids', drawComments);
 
-    this.getComments()
-
+    this.getComments();
   }
 
   async getComments() {
--- client/app/Services/CryptidsService.js
+++ client/app/Services/CryptidsService.js
@@ -1,73 +1,65 @@
-
-import { appState } from "../AppState.js";
-import { ActiveCryptid } from "../Models/ActiveCryptid.js";
-import { Comment } from "../Models/Comment.js";
-import { Cryptid } from "../Models/Cryptid.js";
-import { server } from "./AxiosService.js"
+import { appState } from '../AppState.js';
+import { ActiveCryptid } from '../Models/ActiveCryptid.js';
+import { Comment } from '../Models/Comment.js';
+import { Cryptid } from '../Models/Cryptid.js';
+import { server } from './AxiosService.js';
 
 class CryptidsService {
   async activeCryptid(id) {
-    debugger
-    const res = await server.get(`api/cryptids/${id}`)
+    debugger;
+    const res = await server.get(`api/cryptids/${id}`);
     // console.log(res.data);
-    appState.activeCryptids = new ActiveCryptid(res.data)
-    console.log(appState.activeCryptids);
-
+    appState.activeCryptids = new ActiveCryptid(res.data);
+    // console.log(appState.activeCryptids);
   }
 
   async dislikePost(id) {
-    const res = await server.post(`/api/dislikes`, { id })
+    const res = await server.post(`/api/dislikes`, { id });
     console.log(res.data);
-    let cryptid = appState.cryptids.find(c => c.id == id)
+    let cryptid = appState.cryptids.find((c) => c.id == id);
     // @ts-ignore
     console.log(cryptid);
     // @ts-ignore
-    cryptid.dislikes++
+    cryptid.dislikes++;
     // @ts-ignore
     console.log(cryptid);
-    appState.cryptids = appState.cryptids
+    appState.cryptids = appState.cryptids;
   }
   async getComments(id) {
-
-    const res = await server.get(`/api/cryptids/${id}/comments`)
-    console.log(res.data);
-    appState.comments = res.data.map(c => new Comment(c))
-    console.log(appState.comments);
+    const res = await server.get(`/api/cryptids/${id}/comments`);
+    // console.log(res.data);
+    appState.comments = res.data.map((c) => new Comment(c));
+    // console.log(appState.comments);
   }
 
   async likePost(id) {
-    const res = await server.post(`/api/likes`, { id })
+    const res = await server.post(`/api/likes`, { id });
     console.log(res.data);
-    let cryptid = appState.cryptids.find(c => c.id == id)
+    let cryptid = appState.cryptids.find((c) => c.id == id);
     // @ts-ignore
     // console.log(cryptid);
     // // @ts-ignore
-    cryptid.likes++
+    cryptid.likes++;
     // // @ts-ignore
     // console.log(cryptid);
-    appState.cryptids = appState.cryptids
+    appState.cryptids = appState.cryptids;
   }
-
-
 
   async removeCryptid(id) {
-    await server.delete(`api/cryptids/${id}`)
-    appState.cryptids = appState.cryptids.filter(c => c.id != id)
-    appState.emit('cryptids')
+    await server.delete(`api/cryptids/${id}`);
+    appState.cryptids = appState.cryptids.filter((c) => c.id != id);
+    appState.emit('cryptids');
   }
   async addCryptid(formData) {
-    const res = await server.post('/api/cryptids', formData)
+    const res = await server.post('/api/cryptids', formData);
     // console.log(res.data);
-    appState.cryptids = [...appState.cryptids, new Cryptid(res.data)]
+    appState.cryptids = [...appState.cryptids, new Cryptid(res.data)];
   }
   async getCryptids() {
-    const res = await server.get('/api/cryptids')
+    const res = await server.get('/api/cryptids');
     console.log(res.data);
-    appState.cryptids = res.data.map(c => new Cryptid(c))
+    appState.cryptids = res.data.map((c) => new Cryptid(c));
   }
-
-
-
 }
 
-export const cryptidsService = new CryptidsService()
+export const cryptidsService = new CryptidsService();
